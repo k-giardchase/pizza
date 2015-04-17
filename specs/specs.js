45a2cb448b3451decbee559f3447fac2348fcb0d
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-17 09:51:05 -0700

    Write and pass second test.

--- specs/specs.js
+++ specs/specs.js
@@ -2,10 +2,17 @@ describe('pizza', function() {
   describe('cost', function() {
     it('returns a price amount if a user orders a pizza, regardless of type', function() {
     var testPizza = Object.create(pizza);
-    testPizza.init();
-    testPizza.topping = 0;
     testPizza.cost();
     expect(testPizza.price).to.equal(10);
+    });
+  });
+
+  describe('cost', function() {
+    it('returns a higher price if a user orders a pepperoni pizza than a cheese pizza', function() {
+      var testPizza = Object.create(pizza);
+      testPizza.topping = 2;
+      testPizza.cost();
+      expect(testPizza.price).to.equal(12);
     });
   });
 });
